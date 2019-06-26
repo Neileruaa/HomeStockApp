1e47ee953313ea92e6716bd891623c737ef8e752
Neileruaa <aureliendrey@gmail.com>
2019-06-26 18:37:43 +0200

    Data chercher sur une autre api

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,6 +15,9 @@ class HomeController extends AbstractController
     public function index()
     {
         return $this->render('home/index.html.twig', [
+            'codebar' => null,
+            'nameProduct' => null,
+            'pathImage' => null
         ]);
     }
 
--- src/Service/BarcodeManager.php
+++ src/Service/BarcodeManager.php
@@ -8,30 +8,60 @@ use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\HttpClient\HttpClient;
 
 class BarcodeManager {
-	public function makeHTTPRequest(string $type, string $url) {
+
+    //TODO: Charger une classe quand la classe est crée !
+
+    public function makeHTTPRequest(string $type, string $url) {
 		$httpClient = HttpClient::create();
 		$response = $httpClient->request($type, $url);
+		$status = $response->getStatusCode();
+		if ($status === 404) {
+		    dd('ERROR 404 pas de page trouvé JE DOIS THROW QUELQUE CHOSE');
+        }
 		$content = $response->getContent();
 		return $content;
 	}
 
-	public function getNameOfProduct($barcode) {
-		$crawler = new Crawler($this->makeHTTPRequest('GET', "https://product-open-data.com/gtin/".$barcode));
-		$crawler = $crawler->filter('body');
-		$crawler = $crawler->filter('td')->each(function (Crawler $node, $i){
-			if ($i == 1){
-				preg_match_all('#<b>Commercial name</b> :(.*?) <br>#', $node->html(), $matches);
-				return $matches[1][0];
-			}
-		});
-		return array_filter($crawler)[1];
-	}
+	public function getNameOfProduct($barcode): string
+    {
+        if ($product = $this->getProductFromBarcode($barcode)){
+            return $product['product_name'];
+        } else {
+            $this->crawlNameOfProduct($barcode);
+        }
+    }
 
-	public function getImageOfProduct($barcode) {
+    //Crawl html Bad way
+	public function crawlNameOfProduct ($barcode)
+    {
+        $crawler = new Crawler($this->makeHTTPRequest('GET', "https://product-open-data.com/gtin/".$barcode));
+        $crawler = $crawler->filter('body');
+        $crawler = $crawler->filter('td')->each(function (Crawler $node, $i){
+            if ($i == 1){
+                preg_match_all('#<b>Commercial name</b> :(.*?) <br>#', $node->html(), $matches);
+                return $matches[1][0];
+            }
+        });
+        return array_filter($crawler)[1];
+    }
+
+    //Crawl html Bad way
+    public function getImageOfProduct($barcode) {
 		$crawler = new Crawler($this->makeHTTPRequest('GET', "https://product-open-data.com/gtin/".$barcode));
 		$crawler = $crawler->filter('body');
 		$crawler = $crawler->filter('img');
 
 		return $crawler->getNode(0)->getAttribute('src');
 	}
+
+    //openFoodFact
+    public function getProductFromBarcode($barcode): ?array
+    {
+        $json = $this->makeHTTPRequest('GET', 'http://fr.openfoodfacts.org/api/v0/produit/' . $barcode . '.json');
+        $jsonToArray = json_decode($json, true);
+        if ($jsonToArray['status'] === 1) {
+            return  $jsonToArray['product'];
+        }
+        return null;
+    }
 }
