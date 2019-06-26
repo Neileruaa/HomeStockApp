1d9d0a77a0bbfc94125fa36d44e79e08742235b5
Neileruaa <aureliendrey@gmail.com>
2019-06-27 00:26:34 +0200

    lien nav + beep

--- assets/js/scanner.js
+++ assets/js/scanner.js
@@ -36,8 +36,24 @@ Quagga.init({
     Quagga.start();
 });
 
+var a=new AudioContext() // browsers limit the number of concurrent audio contexts, so you better re-use'em
+
+function beep(vol, freq, duration){
+    var v=a.createOscillator()
+    var u=a.createGain()
+    v.connect(u)
+    v.frequency.value=freq
+    v.type="square"
+    u.connect(a.destination)
+    u.gain.value=vol*0.01
+    v.start(a.currentTime)
+    v.stop(a.currentTime+duration*0.001)
+}
+
+
 
 Quagga.onDetected(function (result) {
+    beep(100,400, 1000);
     console.log("Barcode detected and processed : [" + result.codeResult.code + "]", result);
     document.getElementById('codebar').value = result.codeResult.code;
     const form = document.getElementById('formBarcode')
